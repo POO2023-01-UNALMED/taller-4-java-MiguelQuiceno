8571896d88cd9ea55c071e0d9f8cd721ee375bd6
Miguel Quiceno <miguelangel01yo@gmail.com>
2023-03-03 18:43:13 -0500

    primeros cambios?

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,16 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula = 3;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+        //cedula = 3;
+    }
+    public Persona(){
+        this(0);
     }
 
     public Persona(long cedula, String nombre) {
@@ -31,6 +34,7 @@ public class Persona {
 
     public Persona(String nombre) {
         this.nombre = "";
+        this.cedula = 1;
         totalPersonas++;
     }
     

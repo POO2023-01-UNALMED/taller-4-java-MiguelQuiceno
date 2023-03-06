9bed3f8c2bcd5e964ad7de1abca0c190a391d11e
Miguel Quiceno <miguelangel01yo@gmail.com>
2023-03-06 18:08:36 -0500

    ?2

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -4,9 +4,9 @@ public enum Tipo {
 	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
 	
 	private int codigo;
-	private String nombre;
+	//private String nombre;
 	private Tipo(int codigo,String nombre) {
 		this.codigo = codigo;
-		this.nombre = nombre;
+		//this.nombre = nombre;
 	}
 }
